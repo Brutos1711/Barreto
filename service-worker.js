d731b35010581e3ede67125174b84b916ee712e6
Brutos1711 <barretodasilvabruno1711@gmail.com>
2026-04-22 12:18:19 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,29 @@
-const CACHE_NAME = "barreto-app-v1"
+const CACHE_NAME = "barreto-app-v2"
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ]
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    })
   )
 })
 
